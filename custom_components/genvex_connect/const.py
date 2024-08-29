ab0b095c3c01baa232246c6eda3ce294f5edb5f9
patrick <patrickhairingdahl@gmail.com>
2024-08-29 20:10:05 +0200

    Point to my own fork

--- custom_components/genvex_connect/const.py
+++ custom_components/genvex_connect/const.py
@@ -1,6 +1,6 @@
 """Constants for the Genvex Connect integration."""
 
-DOMAIN = "genvex_connect"
+DOMAIN = "genvex_connect_cts400"
 CONF_DEVICE_ID = "deviceID"
 CONF_AUTHENTICATED_EMAIL = "authenticatedEmail"
 CONF_DEVICE_IP = "deviceIP"
